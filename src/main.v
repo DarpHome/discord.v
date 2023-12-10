807f72c4a148219b5b121e3c61e25894fadb57ff
MCausc78 <mcausc78@gmail.com>
2023-12-10 14:16:12 +0200

    simple square command

--- src/main.v
+++ src/main.v
@@ -2,6 +2,7 @@ module main
 
 import discord
 import os
+import strconv
 import x.json2
 
 fn main() {
@@ -13,11 +14,31 @@ fn main() {
 		if event.name == 'MESSAGE_CREATE' {
 			d := event.data.as_map()
 			channel_id := d['channel_id']! as string
+			message_id := d['id']! as string
 			content := d['content']! as string
-			if content.starts_with('!ping') {
+			if !content.starts_with('!') {
+				return
+			}
+			args := content[1..].split(' ')
+			if args[0] == 'square' {
+				if args.len != 2 {
+					event.creator.request(.post, '/channels/${channel_id}/messages', json: {
+						'content': json2.Any('Specify argument, e.g. !square 7'),
+					})!
+					return
+				}
+				i := strconv.atoi(args[1]) or {
+					event.creator.request(.post, '/channels/${channel_id}/messages', json: {
+						'content': json2.Any('Invalid integer'),
+					})!
+					return 
+				}
 				event.creator.request(.post, '/channels/${channel_id}/messages',
 					json: {
-						'content': json2.Any('pong!')
+						'content': json2.Any((i * i).str())
+						'message_reference': {
+							'message_id': json2.Any(message_id)
+						}
 					}
 				)!
 			}
