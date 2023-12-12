bf26f26f34c6b5e56882f89b7f676a718dfd4ac7
MCausc78 <mcausc78@gmail.com>
2023-12-12 17:49:20 +0200

    impl invite type

--- discord/invite.v
+++ discord/invite.v
@@ -4,6 +4,12 @@ import net.urllib
 import time
 import x.json2
 
+pub enum InviteType {
+	guild
+	group_dm
+	friend	
+}
+
 pub enum InviteTargetType {
 	stream               = 1
 	embedded_application
@@ -12,6 +18,8 @@ pub enum InviteTargetType {
 // Represents a code that when used, adds a user to a guild or group DM channel.
 pub struct Invite {
 pub:
+	// the type of invite
+	typ InviteType
 	// the invite code (unique ID)
 	code string
 	// the guild this invite is for
