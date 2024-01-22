65448b687ae759e385c127c8739b97b6ac62d3e3
MCausc78 <mcausc78@gmail.com>
2024-01-22 22:32:23 +0200

    fix

--- src/permissions.v
+++ src/permissions.v
@@ -100,10 +100,6 @@ pub enum Permissions as u64 {
 	send_voice_messages
 }
 
-pub fn Permissions.zero() Permissions {
-	return unsafe { Permissions(0) }
-}
-
 pub fn Permissions.all() Permissions {
 	part1 := (Permissions.create_instant_invite | .kick_members | .ban_members | .administrator | .manage_channels | .manage_guild | .add_reactions | .view_audit_log | .priority_speaker | .stream | .view_channel | .send_messages | .send_tts_messages | .manage_messages | .embed_links | .attach_files | .read_message_history | .mention_everyone | .use_external_emojis | .view_guild_insights | .connect | .speak | .mute_members | .deafen_members | .move_members | .use_vad | .change_nickname | .manage_nicknames | .manage_roles | .manage_webhooks | .manage_guild_expressions | .use_application_commands | .request_to_speak | .manage_events | .manage_threads | .create_public_threads | .create_private_threads | .use_external_stickers | .send_messages_in_threads)
 	// little hack to make V happy
